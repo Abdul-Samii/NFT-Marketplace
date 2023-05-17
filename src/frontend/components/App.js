37703d277a4806d751743b13adc42feed745a144
Abdul-Samii <ksamk100474@gmail.com>
2023-05-17 02:56:30 -0400

    implement navbar and handle the wallet connection

---
+++ src/frontend/components/App.js
@@ -0,0 +1,64 @@
+import React, { Component, useState } from 'react';
+import './App.css';
+import { ethers } from 'ethers';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+
+import MarketplaceAbi from '../contractsData/Marketplace.json';
+import MarketplaceAddress from '../contractsData/Marketplace-address.json';
+import NFTAbi from '../contractsData/NFT.json';
+import NFTAddress from '../contractsData/NFT-address.json';
+import Navigation from './Navbar';
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [account, setAccount] = useState(null);
+  const [nft, setNFT] = useState({});
+  const [marketplace, setMarketplace] = useState({});
+
+  const web3Handler = async() => {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      setAccount(accounts[0]);
+      if (account) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        loadContracts(signer);
+      } else {
+        alert("No Account found")
+      }
+    } else {
+      alert("Please install metamask")
+    }
+  }
+  
+  const loadContracts = async(signer) => {
+    const marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceAbi.abi, signer)
+    setMarketplace(marketplace);
+    const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer);
+    setNFT(nft);
+  }
+
+    return (
+      <BrowserRouter>
+        <div>
+          <Navigation web3Handler={web3Handler} account={account} />
+          <Routes>
+            <Route path="/" />
+            <Route path="/create" />
+            <Route path="/my-listed-items" />
+            <Route path="/my-purchases" />
+
+          </Routes>
+          <div className="container-fluid mt-5">
+            <div className="row">
+              <main role="main" className="col-lg-12 d-flex text-center">
+                <h3 className='m-auto text-dark'>Welcome to NFT Marketplace</h3>
+              </main>
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+}
+
+export default App;
